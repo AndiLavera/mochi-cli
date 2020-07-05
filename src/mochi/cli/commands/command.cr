eebf56087c0aee9605128729aef767d2a06335ee
Andrew <awcrotwell@gmail.com>
2020-07-05 08:50:56 -0700

    Removed deprecated warnings

--- src/mochi/cli/commands/command.cr
+++ src/mochi/cli/commands/command.cr
@@ -1,17 +1,12 @@
-require "logger"
+require "log"
 
 # :nodoc:
 abstract class Command < Cli::Command
   def info(msg)
-    # Mochi::CLI.logger.info msg, Class.name, :light_cyan
-    log = Logger.new(STDOUT)
-    log.info(msg)
+    Log.info { msg }
   end
 
   def error(msg)
-    # Mochi::CLI.logger.error msg, Class.name, :red
-
-    log = Logger.new(STDOUT)
-    log.error(msg)
+    Log.error { msg }
   end
 end
